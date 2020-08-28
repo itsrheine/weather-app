d510462077c249443087d1363f33a2c61cf6420a
Marrione Nguyen <mtiamsic@gmail.com>
2020-08-28 15:54:29 -0700

    updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,10 +6,42 @@ var humidity = document.querySelector(".humidity");
 var windSpeed = document.querySelector(".windSpeed");
 var uvIndex = document.querySelector(".uvIndex");
 
-button.addEventListener('click', function() {
-    fetch ("https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&appid=386eafe2ba649945a853251bb7d3f25e")
-    .then(response => response.json())
-    .then(data => console.log(data))
+var getCity = function(value) {
+    
+    // format api url
+    var apiKey = "386eafe2ba649945a853251bb7d3f25e";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+value+"&appid="+apiKey;
+    
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
 
-.catch(err => alert("Wrong city name!"))
-})
+        // request was successful
+        if (response.ok) {            
+            response.json().then(function(data) {
+            console.log(data);   
+        })
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    })
+
+    .catch(function(error) {
+        // if above function does not work
+    alert("Unable to connect");
+    });
+};
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    var entryValue = inputValue.value.trim();
+    console.log(entryValue);
+    
+    if (entryValue) {
+        getCity(entryValue);
+        inputValue.value = "";
+        console.log(entryValue);
+    }
+}
+
+button.addEventListener("click", getCity);
