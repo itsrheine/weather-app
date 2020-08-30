92945e27dfb816300e51e3346d92b4179ff6062a
Marrione Nguyen <mtiamsic@gmail.com>
2020-08-30 14:36:36 -0700

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 // variables
 var button = document.querySelector(".button");
 var inputValue = document.querySelector("#city");
-var cityName = document.querySelector(".cityName");
+var cityName = document.querySelector("#searchedCity");
 var currentDay = document.querySelector(".currentDay");
 var weatherDescription = document.querySelector(".description");
 var temperature = document.querySelector(".temperature");
@@ -11,17 +11,7 @@ var uvIndex = document.querySelector(".uvIndex");
 var parent = document.getElementById("parentElementID");
 var recentSearch = document.querySelector("#recentSearch");
 var iconValue = document.querySelector(".weather-icon");
-
-// keyup for recent searches
-inputValue.addEventListener("keyup", () => {
-    // disables the person adding an empty search bar
-    button.disabled = !inputValue.value;
-
-    var city = inputValue.value;
-    getCity(city);
-    cityCoord(city);
-    get5Day(city);
-})
+var inputValTrim = inputValue.value.trim();
 
 var loadLast = function (event) {
     var city = event.target.textContent;
@@ -34,7 +24,7 @@ var loadLast = function (event) {
 var getCity = function(value) {
     
     // format api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+value+"&appid=386eafe2ba649945a853251bb7d3f25e";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+value+"&appid=386eafe2ba649945a853251bb7d3f25e"
 
     // make a request to the url
     fetch(apiUrl).then(function(response) {
@@ -44,168 +34,150 @@ var getCity = function(value) {
             response.json().then(function(data) {
 
             // get all the data - main dashboard
-            var cityValue = data['name'];
-            var m = moment().format('LL');
-    
-            var iconEl = data.weather[0].icon; // this is the code
-            iconValue.setAttribute("src", "http://openweathermap.org/img/w/" + iconEl + ".png");
-            
-            // iconValue.innerHTML = iconEl;
-            var temperatureValue = data['main']['temp'];
-            var temperatureFarhenheit = Math.round(((temperatureValue - 273.15)*1.8)+32) + "°F";
-            var humidityValue = " " + data['main']['humidity'] + "%";
-            var windSpeedValue = " " + data['wind']['speed'] + " mph";             
-
-            cityName.innerHTML = cityValue;
-            currentDay.innerHTML = m;
-            temperature.innerHTML = temperatureFarhenheit;
-            humidity.innerHTML = humidityValue;
-            windSpeed.innerHTML = windSpeedValue;
+            mainDashboard(data, value);
+            cityCoord(data.coord.lon, data.coord.lat);
+            get5Day(data, value);
                 
-        })
+            })
         } else {
             alert("Error: " + response.statusText);
         }
     })
-}
+};
 
+// function for all in dashboard
+var mainDashboard = function(data, city) {
+
+    // city header
+    cityName.textContent = city + " " + moment().format("L") + " ";
+
+    // weather image
+    var iconEl = data.weather[0].icon; // this is the code
+    iconValue.setAttribute("src", "http://openweathermap.org/img/w/" + iconEl + ".png");
+
+    // temperature value
+    var temperatureValue = data['main']['temp'];
+    var temperatureFarhenheit = Math.round(((temperatureValue - 273.15)*1.8)+32) + "°F";
+    temperature.innerHTML = temperatureFarhenheit;
+
+    // humidity value
+    var humidityValue = " " + data['main']['humidity'] + "%";   
+    humidity.innerHTML = humidityValue;
+
+    // wind speed value
+    var windSpeedValue = " " + data['wind']['speed'] + " mph";  
+    windSpeed.innerHTML = windSpeedValue;
+}
 
 // get city's coordinates & uv index for main dashboard
 var cityCoord = function (data) {
+    
+    // variables for longitude and latitude
+    var lonValue = data['coord']['lon'];
+    var latValue = data['coord']['lat'];
 
     // format api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+data+"&appid=386eafe2ba649945a853251bb7d3f25e";
-    
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {    
-
-    // request was successful
-    if (response.ok) {            
-        response.json().then(function(data) {
-    
-        // get remaining data for dashboard
-        var lonValue = data['coord']['lon'];
-        var latValue = data['coord']['lat'];
-
-        // format api url
-        var apiUrl2 = "https://api.openweathermap.org/data/2.5/uvi?appid=386eafe2ba649945a853251bb7d3f25e&lat="+latValue+"&lon="+lonValue;
+    var apiUrl2 = "https://api.openweathermap.org/data/2.5/uvi?appid=386eafe2ba649945a853251bb7d3f25e&lat="+latValue+"&lon="+lonValue;
 
         // make a request to 2nd url
         fetch(apiUrl2).then(function(response) {
             response.json().then(function(data) {
+
+                if (response.ok) {
+                    response.json().then(function(data) {
                 
-                // get data in main dashboard
-                var uvIndexValue = data['value'];
-                uvIndex.innerHTML = uvIndexValue;
+                    // get data in main dashboard
+                    var uvIndexValue = data['value'];
+                    uvIndex.innerHTML = uvIndexValue;
 
-                // conditional statement color coding
-                if (uvIndexValue > 5) {
-                    uvIndex.className = "rounded bg-danger p3";
-                }
-                else if (uvIndexValue < 3) {
-                    uvIndex.className = "rounded bg-success p3";
-                }
-                else {
-                    uvIndex.className = "rounded bg-warning p3";
-                }
-                return uvIndexValue;
+                    // conditional statement color coding
+                    if (uvIndexValue > 5) {
+                        uvIndex.className = "rounded bg-danger p3";
+                    }
+                    else if (uvIndexValue < 3) {
+                        uvIndex.className = "rounded bg-success p3";
+                    }
+                    else {
+                        uvIndex.className = "rounded bg-warning p3";
+                    }
+                    return uvIndexValue;
                 })
-        }); 
-    });  
-
-    } else {
-            alert("Error: " + response.statusText);
+            }
+            else {
+            alert("There is a problem with your request.");
         }
     })
-}
-
+})
 
 // get 5 day forecast
-var get5Day = function (value) {
+var get5Day = function (data) {
 
-    // format api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+value+"&appid=386eafe2ba649945a853251bb7d3f25e";
+    for (var i = 1; 1 < 5; i++) {
 
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        
-        // request was successful
-        if (response.ok) {            
-            response.json().then(function(data) {
-                     
-            for (var i = 1; 1 < 8; i++) {
+        // variables - 5 day forecast
+        var dateEl = document.querySelector("#day" + i);
+        var tempEl = document.querySelector("#temp" + i);
+        var humiEl = document.querySelector("#hum" + i);
+        var iconEl = document.querySelector("#Icon" + i);
 
-                // variables - 5 day forecast
-                var dateEl = document.querySelector("#day" + i);
-                var tempEl = document.querySelector("#temp" + i);
-                var humiEl = document.querySelector("#hum" + i);
-                var iconEl = document.querySelector("#Icon" + i);
+        // secondary dashboard
+        var date = moment().add(i, 'days').format('l');
 
-                // secondary dashboard
-                var date = moment().add(i, 'days').format('l');
-                dateEl.innerHTML = date;
+        dateEl.innerHTML = date;
                 
-                var temperatureValue = data.list[i].main.temp;
-                var temperatureFarhenheit = Math.round(((temperatureValue - 273.15)*1.8)+32) + "°F";
-                tempEl.innerHTML = temperatureFarhenheit;                    
+        var temperatureValue = data.list[i].main.temp;
+        var temperatureFarhenheit = Math.round(((temperatureValue - 273.15)*1.8)+32) + "°F";
+        tempEl.innerHTML = temperatureFarhenheit;                    
                 
-                var humidityValue = data.list[i].main.humidity + "%";
-                humiEl.innerHTML = humidityValue;
+        var humidityValue = data.list[i].main.humidity + "%";
+        humiEl.innerHTML = humidityValue;
 
-                var iconVal = data.list[i].weather[0].icon; // this is the code
-                iconEl.setAttribute("src", "http://openweathermap.org/img/w/" + iconVal + ".png");
-            
-            }
-        })
-        } else {
-        alert("Error: " + response.statusText);
-        }
-    })
-    
-    .catch(function(error) {
-    // if above function does not work
-    alert("Unable to connect");
-    });
-};
+        var iconVal = data.list[i].weather[0].icon; // this is the code
+        iconEl.setAttribute("src", "http://openweathermap.org/img/w/" + iconVal + ".png");
+           
+    }
+}
 
 // when search button is clicked 
 var formSubmitHandler = function(event) {
     event.preventDefault();
 
+    var inputValEl = inputValue.value;
+    
     // city to appear after search
-    var askCity = inputValue.value.trim();
-    if (askCity) {
+    if (inputValTrim) {
         cityList = document.createElement("li");
         cityList.className = "nav-item";
         cityList.innerHTML="<a href='#' class='border nav-link'><span data-feather='file'></span>"+askCity+"</a>";
     
         recentSearch.appendChild(cityList);
+
+        // save to local storage
+        localStorage.setItem("recentSearch", JSON.stringify(newCity));
         
-        getCity(askCity);
-        inputValue.value = "";
-    } else {
+        getCity(inputValEl);
+    } 
+    else {
+
         alert("Please enter a city.");
     }
 
-    var city = inputValue.value;
-    
     // local storage
     function save() {
-        var addCity = city
+        console.log(inputValEl);
         if (localStorage.getItem("recentSearch") === null) {
             localStorage.setItem("recentSearch", "[]");
         }
         var newCity = JSON.parse(localStorage.getItem("recentSearch"));
-        newCity.push(addCity);
-
-        localStorage.setItem("recentSearch", JSON.stringify(newCity))
+        newCity.push(inputValEl);d
     };
 
-    getCity(city);
-    cityCoord(city);
-    get5Day(city);
-    save(city);
+    getCity(inputValEl);
+    cityCoord(inputValEl);
+    get5Day(inputValEl);
+    save(inputValEl);
 };
 
 
 button.addEventListener("click", formSubmitHandler);
+recentSearch.addEventListener("click", loadLast);
